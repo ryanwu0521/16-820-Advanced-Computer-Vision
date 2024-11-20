24b533c093dd300906b0dc69b96764d07804988f
Ryan Wu <weihuanw@andrew.cmu.edu>
2024-11-19 21:39:57 -0500

    Complete all parts of section 2

--- HW5_Photometric_Stereo/src/q2.py
+++ HW5_Photometric_Stereo/src/q2.py
@@ -36,18 +36,28 @@ def estimatePseudonormalsUncalibrated(I):
         The 3 x 7 array of lighting directions
 
     """
+
+    ''' This implementation does not work for Q2.e for some reason.'''
+    # # Perfrom SVD on I
+    # U, S, Vt = np.linalg.svd(I, full_matrices=False)
+
+    # # Truncate the top 3 singular values
+    # S3 = np.diag(S[:3])     # first 3 singular values
+    # U3 = U[:, :3]           # first 3 columns of U
+    # Vt3 = Vt[:3, :]         # first 3 rows of Vt
+
+    # # Calculate lighting directions (L) and pseudonormals (B)
+    # L = U3 @ np.sqrt(S3)
+    # B = np.sqrt(S3) @ Vt3  # B: 3 x P
+
     
+    ''' This implementation works for Q2.e '''
     # Perfrom SVD on I
     U, S, Vt = np.linalg.svd(I, full_matrices=False)
 
-    # Truncate the top 3 singular values
-    S3 = np.diag(S[:3])     # first 3 singular values
-    U3 = U[:, :3]           # first 3 columns of U
-    Vt3 = Vt[:3, :]         # first 3 rows of Vt
-
-    # Calculate lighting directions (L) and pseudonormals (B)
-    L = U3 @ np.sqrt(S3)
-    B = np.sqrt(S3) @ Vt3  # B: 3 x P
+    # Truncate to rank 3 and calculate B and L
+    L = U[:, :3]            # first 3 columns of U
+    B = Vt[:3, :]           # first 3 rows of Vt
    
     return B, L
 
@@ -78,7 +88,30 @@ def plotBasRelief(B, mu, nu, lam):
 
     """
 
-    # Your code here
+    # Construct the bas-relief transformation matrix
+    # G = np.array([[1, 0, mu], [0, 1, nu], [0, 0, lam]])     # Generalized Bas-Relief matrix
+    G = np.array([[1, 0, 0], [0, 1, 0], [mu, nu, lam]])   # Generalized Bas-Relief matrix
+
+    # Apply the bas-relief transformation
+    # B = G @ B
+    B = G.T @ B
+    
+
+    # Compute the albedos and normals
+    albedos, normals = estimateAlbedosNormals(B)
+   
+    # Enforce integrability
+    Nt = enforceIntegrability(normals, s)
+
+    # Estimate the shape
+    surface = estimateShape(Nt, s)
+
+    # Normalize the surface for saving as an image
+    normalized_surface = (surface - np.min(surface)) / (np.max(surface) - np.min(surface))
+
+    # Plot the surface
+    plotSurface(surface, suffix=f"_basrelief_mu{mu}_nu{nu}_lam{lam}")
+    # plotSurface(normalized_surface, suffix=f"_basrelief_mu{mu}_nu{nu}_lam{lam}")
 
 if __name__ == "__main__":
     # Part 2 (b)
@@ -91,25 +124,56 @@ if __name__ == "__main__":
     albedoIm, normalIm = displayAlbedosNormals(albedos, normals, s)
 
     # Save the images
-    plt.imsave("../results/2b-a.png", albedoIm, cmap="gray")
-    plt.imsave("../results/2b-b.png", normalIm, cmap="rainbow")
+    # plt.imsave("../results/2b-a.png", albedoIm, cmap="gray")
+    # plt.imsave("../results/2b-b.png", normalIm, cmap="rainbow")
 
     # Part 2 (c)
-    print('Ground truth lighting directions:\n', L0)
-    print('Estimated lighting directions:\n', L)
+    # print('Ground truth lighting directions:\n', L0)
+    # print('Estimated lighting directions:\n', L)
 
     # Part 2 (d)
-    surface = estimateShape(normals, s)
-    # plotSurface(surface, suffix="_unintegrated")
+    # surface_unenforce = estimateShape(normals, s)
+    # plotSurface(surface_unenforce, suffix="_unintegrated")
 
     # Part 2 (e)
-    Nt = enforceIntegrability(normals, s)
+    # Nt = enforceIntegrability(normals, s)
+    # G = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -1]])    # Generalized Bas-Relief matrix
+    # Nt = G @ Nt
+    # surface_enforce = estimateShape(Nt, s)
+    # plotSurface(surface_enforce, suffix="_integrated")
 
-    # Generalized Bas-Relief
-    G = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -1]])
-    Nt = G @ Nt
-    surface = estimateShape(Nt, s)
-    plotSurface(surface, suffix="_integrated")
 
+# def plotBasRelief(B, mu, nu, lam):
     # Part 2 (f)
-    # Your code here
+
+    # # Test varying mu
+    # plotBasRelief(B, 1, 1, 1)
+    # plotBasRelief(B, 5, 1, 1)
+    # plotBasRelief(B, 10, 1, 1)
+
+    # Test varying nu
+    # plotBasRelief(B, 1, 1, 1)
+    # plotBasRelief(B, 1, 5, 1)
+    # plotBasRelief(B, 1, 10, 1)
+
+    # # Test varying lambda
+    # plotBasRelief(B, 0, 0, 1)
+    # plotBasRelief(B, 0, 0, 5)
+    # plotBasRelief(B, 0, 0, 10)
+
+    
+
+    # Test varying mu
+    # plotBasRelief(B, -1, 0.5 ,1)
+    # plotBasRelief(B, 0.5, 0.5 ,1)
+    # plotBasRelief(B, 1, 0.5 ,1)
+
+    # Test varying nu
+    # plotBasRelief(B, 0.5, -1, 1)
+    # plotBasRelief(B, 0.5, 0.5, 1)
+    # plotBasRelief(B, 0.5, 1, 1)
+
+    # Test varying lambda
+    plotBasRelief(B, 0.5, 0.5, -1)
+    plotBasRelief(B, 0.5, 0.5, 0.5)
+    plotBasRelief(B, 0.5, 0.5, 10)
